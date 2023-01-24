0e5f4942400e9eddb11acee417e61d70184b73c5
rehanulHaque <rehan78khan87@gmail.com>
2023-01-24 12:40:27 +0530

    v1 completed 24/1/2003

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 require("dotenv").config()
 const express = require("express")
+const path = require("path")
 const app = express()
 const PORT = process.env.PORT || 3000
-app.use("/api", pixel)
+
+const random = () =>{
+    return Math.floor(Math.random() * 14  + 1)
+}
+
+
+app.get("/api", async(req, res)=>{
+    res.sendFile(path.join(__dirname + `/arts/${random()}.jpg`))
+})
 
 app.listen(PORT, ()=>{
     console.log(`Server Running on port ${PORT}`)
